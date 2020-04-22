62afdc2b32daf28ce08b284297a39560bf5db90b
André Ferraz <andreferraz25384@gmail.com>
2020-04-22 13:58:46 -0300

    Customized the rest end point for bookCategory

--- onlinebookstore/src/main/java/com/example/onlinebookstore/repositories/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/com/example/onlinebookstore/repositories/BookCategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.onlinebookstore.repositories;
 
 import com.example.onlinebookstore.model.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
