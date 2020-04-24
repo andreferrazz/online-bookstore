62824207139ba0dd7b5c79c3d80ef56ce04c14b4
André Ferraz <andreferraz25384@gmail.com>
2020-04-23 22:03:11 -0300

    Make a HTTP GET request to the spring boot application and display the data

--- angular-bookstore/src/app/app.component.ts
+++ angular-bookstore/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Book } from './common/book';
 
 @Component({
     selector: 'app-root',
@@ -7,6 +6,5 @@ import { Book } from './common/book';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
 
 }
