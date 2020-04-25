770b28eb7a2dbcef268b38127bda9a6e40760944
André Ferraz <andreferraz25384@gmail.com>
2020-04-25 18:50:52 -0300

    Added find by category method in the back-end application

--- onlinebookstore/src/main/java/com/example/onlinebookstore/repositories/BookRepository.java
+++ onlinebookstore/src/main/java/com/example/onlinebookstore/repositories/BookRepository.java
@@ -1,9 +1,14 @@
 package com.example.onlinebookstore.repositories;
 
 import com.example.onlinebookstore.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
