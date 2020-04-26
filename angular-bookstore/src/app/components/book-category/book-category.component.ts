5b97b1edf83a7cbace378b2fc46c6f6c70a5d412
André Ferraz <andreferraz25384@gmail.com>
2020-04-26 19:55:02 -0300

    Modified book category component class to get the category data

--- angular-bookstore/src/app/components/book-category/book-category.component.ts
+++ angular-bookstore/src/app/components/book-category/book-category.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { BookCategory } from 'src/app/common/book-category';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
-  selector: 'app-book-category',
-  templateUrl: './book-category.component.html',
-  styleUrls: ['./book-category.component.css']
+    selector: 'app-book-category',
+    templateUrl: './book-category.component.html',
+    styleUrls: ['./book-category.component.css']
 })
 export class BookCategoryComponent implements OnInit {
 
-  constructor() { }
+    bookCategories: BookCategory[];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private _bookService: BookService
+    ) { }
 
+    ngOnInit(): void {
+        this.listBookCategories();
+    }
+
+    listBookCategories() {
+        this._bookService.getBookCategories().subscribe(
+            data => {
+                console.log("Book categories", data);
+            }
+        );
+    }
 }
