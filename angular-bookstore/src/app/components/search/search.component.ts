a3c6b659a613190d0719cdb8155b342908c2e4fd
André Ferraz <andreferraz25384@gmail.com>
2020-05-10 20:26:46 -0300

    Get the text box value using template reference variable

--- angular-bookstore/src/app/components/search/search.component.ts
+++ angular-bookstore/src/app/components/search/search.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+	selector: 'app-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+	constructor(private _router: Router) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	searchBooks(keyword: string) {
+		console.log('keyword:', keyword);
+		this._router.navigateByUrl('/search/' + keyword);
+	}
 
 }
